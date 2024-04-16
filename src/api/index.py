868f53caa86f7a1d35a1bb1c88a1f0ef35c1bdae
Connor Haines <connor_haines@outlook.com>
2024-04-16 16:33:43 -0600

    testing hypothesis

--- src/api/index.py
+++ src/api/index.py
@@ -1,43 +1,6 @@
-from flask import Flask, request
-
-# from dotenv import load_dotenv
-# import os
-from langchain_openai import ChatOpenAI
-from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
-
-# env = os.environ
-
-
-chat = ChatOpenAI(model="gpt-3.5-turbo-1106", temperature=0.2)
-
-prompt = ChatPromptTemplate.from_messages(
-    [
-        (
-            "system",
-            "You are a helpful assistant. Answer all questions to the best of your ability.",
-        ),
-        MessagesPlaceholder(variable_name="messages"),
-    ]
-)
-
-chain = prompt | chat
-
+from flask import Flask
 
 app = Flask(__name__)
-
-
-@app.route("/api/chat", methods=["POST"])
-def main():
-    test = chain.invoke(
-        {
-            "messages": request.json["messages"],
-        }
-    )
-    try:
-        res = test.content
-    except Exception:
-        res = "Error"
-    return res
 
 @app.route("/api/test", methods=["GET"])
 def test():
